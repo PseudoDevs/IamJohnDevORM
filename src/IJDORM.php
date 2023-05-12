2f6bebafca0a35e1872c6d033a7eb675be3b0885
PseudoDevs <johnlester.mychannelnow@gmail.com>
2023-05-12 12:26:53 +0800

    Using PDO for Preventing SQL Injection Attacks with input Sanitization

--- src/IJDORM.php
+++ src/IJDORM.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace IamJohnDevORM;
 
@@ -17,72 +17,113 @@ class IJDORM {
     }
 
     public function select($table, $fields = "*", $where = "") {
-        $sql = "SELECT $fields FROM $table";
-        if ($where) {
-            $sql .= " WHERE $where";
+        $fields = $this->sanitizeFields($fields);
+        $where = $this->sanitizeWhere($where);
+
+        $stmt = $this->conn->prepare("SELECT $fields FROM $table WHERE $where");
+        if ($stmt === false) {
+            throw new Exception("Error preparing SQL statement: " . $this->conn->error);
         }
-
-        $result = $this->conn->query($sql);
-
-        if (!$result) {
-            throw new Exception("Error: " . $this->conn->error);
+        
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result === false) {
+            throw new Exception("Error executing SQL query: " . $this->conn->error);
         }
-
+        
         $rows = [];
         while ($row = $result->fetch_assoc()) {
-            $rows[] = $row;
+            $rows[] = $this->sanitizeData($row);
         }
-
+        
         return $rows;
     }
 
     public function insert($table, $data) {
+        $data = $this->sanitizeData($data);
+
         $keys = implode(",", array_keys($data));
-        $values = "'" . implode("','", array_values($data)) . "'";
+        $values = implode(",", array_fill(0, count($data), "?"));
 
         $sql = "INSERT INTO $table ($keys) VALUES ($values)";
+        $params = array_values($data);
 
-        return $this->executeQuery($sql);
+        return $this->executePreparedStatement($sql, $params);
     }
 
-    public function update($table, $data, $where = "") {
+    public function update($table, $data, $where = "", $params = []) {
+        $data = $this->sanitizeData($data);
+        $where = $this->sanitizeWhere($where);
+
         $set = [];
         foreach ($data as $key => $value) {
-            $set[] = "$key = '$value'";
+            $set[] = "$key = ?";
+            $params[] = $value;
         }
 
-        $set = implode(",", $set);
-
-        $sql = "UPDATE $table SET $set";
+        $sql = "UPDATE $table SET " . implode(",", $set);
         if ($where) {
             $sql .= " WHERE $where";
         }
 
-        return $this->executeQuery($sql);
+        return $this->executePreparedStatement($sql, $params);
     }
 
-    public function delete($table, $where = "") {
+    public function delete($table, $where = "", $params = []) {
+        $where = $this->sanitizeWhere($where);
+
         $sql = "DELETE FROM $table";
         if ($where) {
             $sql .= " WHERE $where";
         }
 
-        return $this->executeQuery($sql);
+        return $this->executePreparedStatement($sql, $params);
     }
 
-    private function executeQuery($sql) {
-        $result = $this->conn->query($sql);
-
-        if (!$result) {
-            throw new Exception("Error: " . $this->conn->error);
+    private function executePreparedStatement($sql, $params) {
+        $stmt = $this->conn->prepare($sql);
+        if (!$stmt) {
+            throw new Exception("Error preparing statement: " . $this->conn->error);
         }
 
-        return $result;
+        if ($params) {
+            $types = str_repeat('s', count($params));
+            $stmt->bind_param($types, ...$params);
+        }
+
+        if (!$stmt->execute()) {
+            throw new Exception("Error executing statement: " . $stmt->error);
+        }
+
+        $affected_rows = $stmt->affected_rows;
+        $stmt->close();
+
+        return $affected_rows;
     }
 
+    private function sanitizeFields($fields) {
+        $fields = preg_replace("/[^a-zA-Z0-9_,*]/", "", $fields);
+        return $fields;
+    }
+
+    private function sanitizeWhere($where) {
+        if (!$where) {
+        return "";
+        }
+        $where = preg_replace("/[^a-zA-Z0-9_=\s]/", "", $where);
+        return $where;
+    }
+    
+    private function sanitizeData($data) {
+        foreach ($data as $key => $value) {
+            $data[$key] = $this->conn->real_escape_string($value);
+        }
+    
+        return $data;
+    }
+    
     public function __destruct() {
-        if ($this->conn) {
-            $this->conn->close();
-        }
+        $this->conn->close();
     }
 }
